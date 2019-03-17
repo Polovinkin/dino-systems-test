96029876b87ad955be31bc3172ea2459b06500b0
Dmitry <dtpolovinkin@gmail.com>
2019-03-17 17:48:48 +0300

    finished

--- total-expenses.sql
+++ total-expenses.sql
@@ -1,4 +1,4 @@
 -- Total expenses
 select sum(timestampdiff(second, Timestamp_start, Timestamp_end)) * 0.04 as total_expenses 
-from call_logs cl
+from call_logs cl 
 where Call_dir = "out" and cl.to not in (select Phone_number from numbers);
